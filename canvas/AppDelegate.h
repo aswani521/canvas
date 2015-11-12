faad059d8abef68b3b43de3b94c4e507815a1c90
Aswani Nerella <aswani521@gmail.com>
2015-11-12 14:34:18 -0800

    Initial Commit

---
+++ canvas/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  canvas
+//
+//  Created by Aswani Nerella on 11/12/15.
+//  Copyright © 2015 Aswani Nerella. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ canvas/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  canvas
+//
+//  Created by Aswani Nerella on 11/12/15.
+//  Copyright © 2015 Aswani Nerella. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
