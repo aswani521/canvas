61bf2721295eaf84a04131b10f116fd58d02e7ee
Aswani Nerella <aswani521@gmail.com>
2015-11-12 18:57:23 -0800

    Canvas - Codepath Lab-week4

--- canvas/ViewController.m
+++ canvas/ViewController.m
@@ -9,13 +9,54 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIView *trayView;
+@property (nonatomic, assign) CGPoint trayOriginalCenter;
+@property (nonatomic, assign) CGPoint trayCenterOpen;
+@property (nonatomic, assign) CGPoint trayCenterClosed;
+// Down = NO, Up = YES;
+@property (nonatomic, assign) BOOL direction;
 
 @end
 
 @implementation ViewController
+- (IBAction)onTrayPanGesture:(UIPanGestureRecognizer *)sender {
+    // Absolute (x,y) coordinates in parentView
+    CGPoint translatedLocation = [sender translationInView:self.view];
+    
+    if (sender.state == UIGestureRecognizerStateBegan) {
+        self.trayOriginalCenter = self.trayView.center;
+
+//        NSLog(@"Gesture began at: %@", NSStringFromCGPoint(self.trayOriginalCenter));
+    } else if (sender.state == UIGestureRecognizerStateChanged) {
+//        NSLog(@"Gesture changed at: %@", NSStringFromCGPoint(location));
+        NSLog(@"Velocity: %@", NSStringFromCGPoint([sender velocityInView:self.view]));
+        
+        if ([sender velocityInView:self.view].y > 0) {
+            self.direction = NO;
+        } else {
+            self.direction = YES;
+        }
+        self.trayView.center = CGPointMake(self.trayOriginalCenter.x, self.trayOriginalCenter.y + translatedLocation.y);
+        
+    } else if (sender.state == UIGestureRecognizerStateEnded) {
+//        NSLog(@"Gesture ended at: %@", NSStringFromCGPoint(location));
+                self.trayView.center = CGPointMake(self.trayOriginalCenter.x, self.trayOriginalCenter.y + translatedLocation.y);
+        
+        if (self.direction) {
+            self.trayView.center = self.trayCenterOpen;
+        } else {
+            self.trayView.center = self.trayCenterClosed;
+        }
+    }
+    
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    self.trayCenterOpen = self.trayView.center;
+    
+    self.trayCenterClosed = CGPointMake(self.trayView.center.x, CGRectGetMaxY(self.view.frame) + 70);
     // Do any additional setup after loading the view, typically from a nib.
 }
 
